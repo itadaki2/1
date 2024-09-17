c92d56a2e8fc3a11032522a1d3c7ff4883c0ecbc
shdwvrs <yakyzaanengs22@gmail.com>
2024-09-17 12:40:23 +0900

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,29 @@
+fun main() {
+    // Целочисленные значения
+    val age: Int = 18
+    val population: Long = 7_900_000_000L
+    val temperature: Short = -5
+    val maximumByte: Byte = 127
+
+    // Числа с плавающей точкой
+    val pi: Double = 3.14159
+    val e: Float = 2.718f
+
+    // Символ и строка
+    val letter: Char = 'K'
+    val greeting: String = "ПРИВЕТ МИР"
+
+    // Логическое значение
+    val kotlinfalse: Boolean = false
+
+    // Вывод значений
+    println("Возраст: $age")
+    println("Население планеты: $population")
+    println("Температура: $temperature")
+    println("Максимальное значение байта: $maximumByte")
+    println("Число Пи: $pi")
+    println("Число e: $e")
+    println("Буква: $letter")
+    println("Приветствие: $greeting")
+    println("Котлин хороший язык? $kotlinfalse")
+}
